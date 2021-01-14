d8e48f78e2cfe5d2346a121e6b6e6a6c1817608e
Patrick Dang <pdang2@mail.greenriver.edu>
2021-01-14 15:23:37 -0500

    index.php added

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the auto autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('Debug',3);
+
+//Run fat free
+$f3->run();
